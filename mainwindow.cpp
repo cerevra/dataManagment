26996944152fb9668f8a35370754b09e70ae6a2d
klevanets <cerevra@gmail.com>
2014-09-04 13:39:16 +0400

    есть основные компоненты интерфейса

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,7 @@
+#include <QStyleFactory>
+#include <QLabel>
+#include <QRect>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,9 +10,43 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QApplication::setStyle("Fusion");
+    onKpCountSpinChanged(ui->spinBox_kpCount->value());
+
+    connect(ui->spinBox_kpCount, SIGNAL(valueChanged        (int)) ,
+            this               , SLOT  (onKpCountSpinChanged(int)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::onKpCountSpinChanged(int count)
+{
+    m_kpCount = count;
+
+    for(int i = 0; i < m_kpCapacitiesSpins.size(); ++i)
+    {
+        QDoubleSpinBox* spinBox = m_kpCapacitiesSpins[i];
+        ui->formLayout->labelForField(spinBox)->deleteLater();
+        spinBox->deleteLater();
+    }
+    m_kpCapacitiesSpins.clear();
+
+    for(int i = 0; i < count; ++i)
+    {
+        QDoubleSpinBox* spinBox = new QDoubleSpinBox();
+        spinBox->setValue(2);
+        ui->formLayout->addRow(QString("№%1").arg(i+1),spinBox);
+        m_kpCapacitiesSpins.append(spinBox);
+    }
+
+    QRect geometry = ui->widget_kpCapacities->geometry();
+    ui->widget_kpCapacities->setGeometry(
+        QRect(geometry.topLeft(),QSize(geometry.width(),
+            (ui->spinBox_kpCount->height()+ui->formLayout->verticalSpacing()
+              +20 // из-за разницы размеров виджета и formLayout
+              )*count)));
+}
+//ui->scrollArea->scroll(0,500);
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDoubleSpinBox>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void onKpCountSpinChanged(int count);
+
 private:
     Ui::MainWindow *ui;
+
+    int                    m_arcCount;
+    qreal                  m_arcCapacity;
+    int                    m_kpCount;
+    QList<qreal>           m_kpCapacities;
+    QList<QDoubleSpinBox*> m_kpCapacitiesSpins;
 };
 
 #endif // MAINWINDOW_H
